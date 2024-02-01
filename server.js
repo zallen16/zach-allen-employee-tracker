14af413aef0b3fc5e8cb0fbe6326c6f6d2a5eb94
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-02-01 18:56:43 -0500

    created boilerplate code for server.js

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const inquirer = require("inquirer");
 const mySql = require("mysql2");
 
+const PORT = express.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_management_db'
+    },
+    console.log(`Connected to the employee_management_db database.`)
+);
+
 const mainMenu = [
     {
         name: 'mainMenu',
