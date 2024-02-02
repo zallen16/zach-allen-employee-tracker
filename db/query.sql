fa34328e77ec5cfc253fb43428c7194417947597
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-02-02 00:02:39 -0500

    finished .then statement for main menu

---
+++ db/query.sql
@@ -0,0 +1,2 @@
+SELECT 
+FROM department
--- server.js
+++ server.js
@@ -33,24 +33,49 @@ function start() {
             'Update an employee role',
             ],
         })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addaDepartment();
+                    break;
+                case "Add a role":
+                    addaRole();
+                    break;
+                case "Add an employee":
+                    addAnEmployee();
+                    break;
+                case "Update an employee role":
+                    updateRole();
+                    break;
+            }
+        });
         }
 
-const mainMenu = [
-    {
-        name: 'mainMenu',
-        type: 'list', 
-        message: 'What would you like to do?',
-        choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role'
-        ]
-    }
-];
+// const mainMenu = [
+//     {
+//         name: 'mainMenu',
+//         type: 'list', 
+//         message: 'What would you like to do?',
+//         choices: [
+//             'View all departments',
+//             'View all roles',
+//             'View all employees',
+//             'Add a department',
+//             'Add a role',
+//             'Add an employee',
+//             'Update an employee role'
+//         ]
+//     }
+// ];
 
 function viewAllDepartments() {
     const query = "SELECT * FROM departments";
@@ -61,3 +86,6 @@ function viewAllDepartments() {
     })
 }
 
+function viewAllRoles () {
+    const query = ""
+}
