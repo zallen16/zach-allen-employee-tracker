f47b658b9d028b0b50952857c1235504bbae9836
zallen16 <126983111+zallen16@users.noreply.github.com>
2024-02-02 00:33:51 -0500

    added other functions for queries

--- server.js
+++ server.js
@@ -78,14 +78,47 @@ function start() {
 // ];
 
 function viewAllDepartments() {
-    const query = "SELECT * FROM departments";
+    const query = "SELECT * FROM department";
     db.query(query, (err, res) => {
         if (err) throw err;
         console.table(res);
         start();
-    })
+    });
 }
 
 function viewAllRoles () {
-    const query = ""
+    const query = "SELECT role.title, role.id, department.department_name, role.salary"
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
 }
+
+function viewAllEmployees () {
+    const query = "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department_name, role.salary, employee.manager_id"
+    db.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
+}
+
+function addDepartment () {
+    inquirer
+        .prompt({
+            type: "input",
+            name: "name",
+            message: "Enter the name of the new department:",
+        })
+        .then((answer) => {
+            console.log(answer.name)
+            const query = `INSERT INTO department VALUE ("${answer.name}")`;
+            db.query(query, (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                start();
+            });
+        })
+}
+
